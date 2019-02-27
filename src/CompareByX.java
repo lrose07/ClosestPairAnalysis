66675632667fc8cda0da377a11b6b71cf0c9adc7
Lauren Rose <larose@radford.edu>
2019-02-26 21:53:46 -0500

    WIP divide/con logic

--- src/CompareByX.java
+++ src/CompareByX.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
 
 public class CompareByX implements Comparator<AlgyPoint> {
     public int compare(AlgyPoint a, AlgyPoint b) {
-        return Integer.compare(a.getX(), b.getY());
+        return Integer.compare(a.getX(), b.getX());
     }
 }
--- src/DivideConquerSolver.java
+++ src/DivideConquerSolver.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 class DivideConquerSolver {
 
     private AlgyPoint[] allPoints;
+    private AlgyPoint[] currentClosestPair = new AlgyPoint[2];
 
     DivideConquerSolver(String inputStream) {
         parseInput(inputStream);
@@ -40,12 +41,13 @@ class DivideConquerSolver {
     private void runAlgorithm() {
         AlgyPoint[] xSorted = sortPointsByX();
         AlgyPoint[] ySorted = sortPointsByY();
+
         closestPair(xSorted, ySorted);
     }
 
     private AlgyPoint[] closestPair(AlgyPoint[] sortedXArray, AlgyPoint[] sortedYArray) {
         int totalDataSize = sortedXArray.length;
-        int midpoint = sortedXArray.length / 2;
+        int midpoint = totalDataSize / 2;
 
         AlgyPoint[] leftXSorted = new AlgyPoint[midpoint];
         AlgyPoint[] rightXSorted = new AlgyPoint[midpoint];
@@ -58,51 +60,75 @@ class DivideConquerSolver {
         } else {
             System.arraycopy(sortedXArray, 0,
                     leftXSorted, 0, leftXSorted.length);
-            System.arraycopy(sortedXArray, midpoint + 1,
+            System.arraycopy(sortedXArray, (sortedXArray.length / 2),
                     rightXSorted, 0, rightXSorted.length);
+
             // distribute/copy the same points from leftXSorted into leftYSorted
             int index = 0;
             for (AlgyPoint point : sortedYArray) {
                 for (AlgyPoint innerPoint : leftXSorted) {
-                    if (innerPoint.getX() == point.getX()) {
+                    if ((innerPoint.getX() == point.getX()) && (innerPoint.getY() == point.getY())) {
                         leftYSorted[index] = innerPoint;
+                        index++;
                     }
                 }
-                index++;
             }
+
             // distribute/copy the same points from rightXSorted into rightYSorted
             index = 0;
             for (AlgyPoint point : sortedYArray) {
                 for (AlgyPoint innerPoint : rightXSorted) {
-                    if (innerPoint.getX() == point.getX()) {
+                    if ((innerPoint.getX() == point.getX()) && innerPoint.getY() == point.getY()) {
                         rightYSorted[index] = innerPoint;
+                        index++;
                     }
                 }
-                index++;
             }
 
-            AlgyPoint[] closestLeft = closestPair(leftXSorted, leftYSorted);
-            AlgyPoint[] closestRight = closestPair(rightXSorted, rightYSorted);
+            closestPair(leftXSorted, leftYSorted);
+            closestPair(rightXSorted, rightYSorted);
 
-            double distanceLeft = getDistance(closestLeft[0], closestLeft[1]);
-            double distanceRight = getDistance(closestRight[0], closestRight[1]);
+            System.out.println(currentClosestPair[0]);
 
-            double distance = Math.min(distanceLeft, distanceRight);
-            double distanceSquared = Math.pow(distance, 2);
-
-            int middlePointX = sortedXArray[midpoint].getX();
-
-            ArrayList<AlgyPoint> slab = new ArrayList<>();
-            for (AlgyPoint point : sortedYArray) {
-                if (Math.abs(point.getX()) > (middlePointX - distance) ||
-                        Math.abs(point.getX()) < (middlePointX + distance)) {
-                    slab.add(point);
-                }
-            }
+//            double distanceLeft = getDistance(closestLeft[0], closestLeft[1]);
+//            double distanceRight = getDistance(closestRight[0], closestRight[1]);
+//
+//            if (distanceLeft >= distanceRight) {
+//                currentClosestPair[0] = closestLeft[0];
+//                currentClosestPair[1] = closestLeft[1];
+//            } else {
+//                currentClosestPair[0] = closestRight[0];
+//                currentClosestPair[1] = closestRight[1];
+//            }
+//
+//            double distance = Math.min(distanceLeft, distanceRight);
+//            double distanceSquared = Math.pow(distance, 2);
+//
+//            int middlePointX = sortedXArray[midpoint].getX();
+//
+//            ArrayList<AlgyPoint> slab = new ArrayList<>();
+//            for (AlgyPoint point : sortedYArray) {
+//                if (Math.abs(point.getX()) > (middlePointX - distance) ||
+//                        Math.abs(point.getX()) < (middlePointX + distance)) {
+//                    slab.add(point);
+//                }
+//            }
+//
+//            int arrayCheckOffset = 7; // number of points to check above current point
+//
+//            for (int i = 0; i < slab.size() - arrayCheckOffset; i++) {
+//                for (int j = i; j < i + arrayCheckOffset; j++) {
+//                    double tempDistance = getDistance(slab.get(i), slab.get(j));
+//                    if (tempDistance < distance) {
+//                        distance = tempDistance;
+//                        currentClosestPair[0] = slab.get(i);
+//                        currentClosestPair[1] = slab.get(j);
+//                    }
+//                }
+//            }
         }
 
-        AlgyPoint[] temp = new AlgyPoint[5];
-        return temp;
+        return currentClosestPair;
     }
 
     private double getDistance(AlgyPoint a, AlgyPoint b) {
